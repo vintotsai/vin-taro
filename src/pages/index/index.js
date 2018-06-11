20539987a5f90e0be1a61a89563a81a2f64451c1
vincentotsai <vintotsai@gmail.com>
2018-06-11 11:26:46 +0800

    init: h5weapp

---
+++ src/pages/index/index.js
@@ -0,0 +1,28 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmout () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>taro!the terminator.</Text>
+      </View>
+    )
+  }
+}
+
